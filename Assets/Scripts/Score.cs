19d94576040a698c8c6439bc3b2578dbc67cbad8
ArisatoX <patricksungkharisma27@gmail.com>
2021-01-06 19:32:58 +0700

    score 0/50

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,10 +7,12 @@ public class Score : MonoBehaviour {
 
     public int score;
     public Text scoreDisplay;
+    public int limit;
 
     private void Update()
     {
-        scoreDisplay.text = score.ToString();
+        string scoreText = score.ToString() + "/" + limit;
+        scoreDisplay.text = scoreText;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
